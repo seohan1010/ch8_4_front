4f603336cab0f599569321e93ec7c4b3560ae54d
seohan1010 <seohan1010@hotmail.com>
2023-09-22 02:02:13 +0900

    list fetch test complete condition

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import classes from './Navigation.module.css';
+
+const Navigation =() => {
+
+return(
+<>
+    <div className={classes.navigation_wrap}>
+
+            <div className={classes.title}>ch8_4</div>
+
+    </div>
+</>
+);
+
+}
+
+
+export default Navigation;
